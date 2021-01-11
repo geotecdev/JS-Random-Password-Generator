0bf3d53a5e1689c954009e59d27c403a7847c0e5
Mike Ruane <mikeruanedev@gmail.com>
2021-01-11 15:01:04 -0500

    add function to generate random pw with input as args

--- script.js
+++ script.js
@@ -4,15 +4,8 @@ var passwordText = document.querySelector("#password");
 
 // Write password to the #password input
 function writePassword() {
-  //var password = generatePassword();
-  var password = "";
-  var passwordText = document.querySelector("#password");
 
-  //set generated password as content
-  passwordText.value = password;
-
-  
-  //choose number of characters between 8 and 128
+  //propmt user to choose a number of characters between 8 and 128
   var charCount = getCharCount();
 
   //if char count is null (because user pressed cancel) exit function
@@ -32,8 +25,11 @@ function writePassword() {
   //include special chars y/n
   var hasSpecialChars = confirm("should password include special characters?");
 
-  
+  //call generatePassword function with input as args
+  var password = generatePassword(charCount, hasLowercase, hasUppercase, hasNumbers, hasSpecialChars);
 
+  //set generated password as content
+  passwordText.value = password;
 }
 
 //prompt and validation functions //////////////////
@@ -54,9 +50,11 @@ function getCharCount() {
   
       //parse number and continue validation
       charCount = parseInt(charCount);
+      //make sure number entry is numeric
       if (charCount != null && (charCount === 0 || isNaN(charCount))) {
         alert("please enter a valid number");
       }
+      //make sure numeric input is in the proper range
       else if (charCount < 8 || charCount > 128) {
         alert("character count must be between 8 and 128");
       }
@@ -67,6 +65,45 @@ function getCharCount() {
     
     return charCount;
   }
+
+
+//generatePassword func
+function generatePassword(charCount, hasLowercase, hasUppercase, hasNumbers, hasSpecialChars) {
+  var password = "";
+
+  var lcChars = "abcdefghijklmnopqrstuvwxyz";
+  var ucChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numChars = "0123456789";
+  var specialChars = "~!@#$%^&*?";
+
+  //concat list of valid chars for password based on arguments passed in
+  var allChars = "";
+  if (hasLowercase) {
+    allChars += lcChars;
+  }
+  if (hasUppercase) {
+    allChars += ucChars;
+  }
+  if (hasNumbers) {
+    allChars += numChars;
+  }
+  if (hasSpecialChars) {
+    allChars += specialChars;
+  }
+
+  //if concat string is empty, alert user that they need to select at least 1 type
+  if (allChars === "") {
+    alert("no character types were choosen. You need to select 'yes' for at least one character type. Please reset password criteria.");
+    return password;
+  }
+
+  //generate random password
+  for (var i = 0; i < charCount; i++) {
+    password += allChars.charAt(Math.floor(Math.random() * allChars.length));
+  }
+
+  return password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
